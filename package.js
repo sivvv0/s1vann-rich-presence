641fae1472e93915b70f1fa2935ed7e9ba89a56a
s1vann <140653998+sivvv0@users.noreply.github.com>
2026-06-01 14:46:42 +0300

    Update package.js

--- package.js
+++ package.js
@@ -4,10 +4,20 @@
   "description": "Modern Discord Rich Presence framework",
   "main": "index.js",
   "type": "commonjs",
+  "private": false,
+  "author": "s1vann  sivandev@tuta.io",
   "license": "MIT",
   "engines": {
     "node": ">=18"
   },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/sivvv0/s1vann-rich-presence.git"
+  },
+  "bugs": {
+    "url": "https://github.com/sivvv0/s1vann-rich-presence/issues"
+  },
+  "homepage": "https://github.com/sivvv0/s1vann-rich-presence#readme",
   "keywords": [
     "discord",
     "discord-rpc",
